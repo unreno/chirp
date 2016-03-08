38d569e1fbc98e30ca40aa00d5b4dd310293ecf6
Jake <github@jakewendt.com>
2016-03-08 11:19:06 -0800

    Extracted core SQL into own file.

--- production/registry_init.bash
+++ production/registry_init.bash
@@ -5,46 +5,7 @@
 #	just catting them.
 
 
-cat << EOF
-
--- MS Sets these before every “CREATE TRIGGER”
--- Not sure if calling them once will suffice.
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-
-IF db_id('chirp') IS NOT NULL
-	DROP DATABASE chirp;
-
--- "WITH TRUSTWORTHY ON" required for use of tSQLt Testing Framework.
-CREATE DATABASE chirp WITH TRUSTWORTHY ON;
-GO
-USE chirp;
-
---Wanted to see these Database Diagrams and this seemed to work.
-ALTER AUTHORIZATION ON DATABASE::chirp TO [sa];
-
-IF OBJECT_ID('debug_log', 'U') IS NOT NULL
-	DROP TABLE debug_log;
-CREATE TABLE debug_log ( message text, logged_at DATETIME DEFAULT CURRENT_TIMESTAMP );
-GO
-IF OBJECT_ID ( 'log', 'P' ) IS NOT NULL
-	DROP PROCEDURE log;
-GO
-CREATE PROCEDURE log(@msg TEXT)
-AS
-BEGIN
-	-- SET NOCOUNT ON added to prevent extra result sets from
-	-- interfering with SELECT statements.
-	SET NOCOUNT ON;
-	INSERT INTO debug_log ( message )
-	VALUES ( @msg );
-END
-GO
-
-EOF
-
+cat create_core_database.tsql
 
 cat create_identifiers.tsql
 
