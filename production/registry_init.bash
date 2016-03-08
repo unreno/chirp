44dc3685440f62f641af0dcf07691450a748bc1c
Jake <github@jakewendt.com>
2016-03-08 09:40:23 -0800

    Included tSQLt source code.

--- production/registry_init.bash
+++ production/registry_init.bash
@@ -51,3 +51,11 @@ cat create_identifiers.tsql
 cat create_warehouse.tsql
 
 cat create_vital_records.tsql
+
+#
+#	Include the testing framework?
+#
+#cat ../tSQLt_V1.0.5873.27393/tSQLt.class.sql
+#
+
+
