11e81ead5b970396c8adc72df252da2c35765edf
Jake <github@jakewendt.com>
2016-03-11 09:28:07 -0800

    Stopped using bulk column concept codes. Added units to weight and noted unpivot unnecesary here.

--- production/create_warehouse_procedures.sql
+++ production/create_warehouse_procedures.sql
@@ -15,14 +15,16 @@ BEGIN
 	SET NOCOUNT ON;
 
 	INSERT INTO observations
-		(chirp_id, provider_id, location_id, started_at, value_type, concept, s_value, downloaded_from, downloaded_at) 
-		SELECT chirp_id, provider_id, location_id, started_at, value_type, concept, s_value, downloaded_from, downloaded_at
+		(chirp_id, provider_id, location_id, started_at, value_type, 
+			concept, s_value, downloaded_from, downloaded_at) 
+		SELECT chirp_id, provider_id, location_id, started_at, value_type, 
+			concept, s_value, downloaded_from, downloaded_at
 		FROM (
 			SELECT i.chirp_id, b.date_of_birth AS started_at, 
 				'123' AS provider_id,
 				'456' AS location_id,
 				'S' as value_type,
-				CONVERT(VARCHAR(255), b.sex) AS [birth:sex],
+				CONVERT(VARCHAR(255), b.sex) AS [DEM:Sex],
 				'The State' AS downloaded_from,
 				b.imported_at AS downloaded_at
 			FROM vital_records.birth b
@@ -34,18 +36,20 @@ BEGIN
 			WHERE b.imported_to_dw = 'FALSE'
 		) arbitraryrequiredandignoredname
 		UNPIVOT (
-			s_value FOR concept IN ( [birth:sex] )
+			s_value FOR concept IN ( [DEM:Sex] )
 		) AS anotherarbitraryrequiredname
 
 	INSERT INTO observations
-		(chirp_id, provider_id, location_id, started_at, value_type, concept, t_value, downloaded_from, downloaded_at) 
-		SELECT chirp_id, provider_id, location_id, started_at, value_type, concept, t_value, downloaded_from, downloaded_at
+		(chirp_id, provider_id, location_id, started_at, value_type, 
+			concept, t_value, downloaded_from, downloaded_at) 
+		SELECT chirp_id, provider_id, location_id, started_at, value_type, 
+			concept, t_value, downloaded_from, downloaded_at
 		FROM (
 			SELECT i.chirp_id, b.date_of_birth AS started_at, 
 				'123' AS provider_id,
 				'456' AS location_id,
 				'T' as value_type,
-				b.date_of_birth AS [birth:date_of_birth],
+				b.date_of_birth AS [DEM:DOB],
 				'The State' AS downloaded_from,
 				b.imported_at AS downloaded_at
 			FROM vital_records.birth b
@@ -57,32 +61,59 @@ BEGIN
 			WHERE b.imported_to_dw = 'FALSE'
 		) arbitraryrequiredandignoredname
 		UNPIVOT (
-			t_value FOR concept IN ( [birth:date_of_birth] )
+			t_value FOR concept IN ( [DEM:DOB] )
 		) AS anotherarbitraryrequiredname
 
+--	INSERT INTO observations
+--		(chirp_id, provider_id, location_id, started_at, value_type, 
+--			concept, n_value, downloaded_from, downloaded_at) 
+--		SELECT chirp_id, provider_id, location_id, started_at, value_type, 
+--			concept, n_value, downloaded_from, downloaded_at
+--		FROM (
+--			SELECT i.chirp_id, b.date_of_birth AS started_at, 
+--				'123' AS provider_id,
+--				'456' AS location_id,
+--				'N' as value_type,
+--				(CAST(b.birth_weight_lbs AS FLOAT) + (CAST(b.birth_weight_oz AS FLOAT)/16)) AS [DEM:Weight],
+--				'The State' AS downloaded_from,
+--				b.imported_at AS downloaded_at
+--			FROM vital_records.birth b
+--			JOIN private.identifiers i
+--				ON i.source_id = b.state_file_number
+--				AND i.source_column = 'state_file_number'
+--				AND i.source_table = 'birth'
+--				AND i.source_schema = 'vital_records'
+--			WHERE b.imported_to_dw = 'FALSE'
+--		) arbitraryrequiredandignoredname
+--		UNPIVOT (
+--			n_value FOR concept IN ( [DEM:Weight] )
+--		) AS anotherarbitraryrequiredname
+
+
+--	The UNPIVOT seems really unnecessary if only doing 1 column.
+
 	INSERT INTO observations
-		(chirp_id, provider_id, location_id, started_at, value_type, concept, n_value, downloaded_from, downloaded_at) 
-		SELECT chirp_id, provider_id, location_id, started_at, value_type, concept, n_value, downloaded_from, downloaded_at
-		FROM (
-			SELECT i.chirp_id, b.date_of_birth AS started_at, 
-				'123' AS provider_id,
-				'456' AS location_id,
-				'N' as value_type,
-				b.birth_weight_lbs AS [birth:birth_weight_lbs],
-				b.birth_weight_oz AS [birth:birth_weight_oz],
-				'The State' AS downloaded_from,
-				b.imported_at AS downloaded_at
-			FROM vital_records.birth b
-			JOIN private.identifiers i
-				ON i.source_id = b.state_file_number
-				AND i.source_column = 'state_file_number'
-				AND i.source_table = 'birth'
-				AND i.source_schema = 'vital_records'
-			WHERE b.imported_to_dw = 'FALSE'
-		) arbitraryrequiredandignoredname
-		UNPIVOT (
-			n_value FOR concept IN ( [birth:birth_weight_lbs], [birth:birth_weight_oz] )
-		) AS anotherarbitraryrequiredname
+		(chirp_id, provider_id, location_id, started_at, value_type, 
+			concept, n_value, units, downloaded_from, downloaded_at) 
+		SELECT i.chirp_id, 
+			'123' AS provider_id,
+			'456' AS location_id,
+			b.date_of_birth AS started_at, 
+			'N' as value_type,
+			'DEM:Weight' AS concept,
+			(CAST(b.birth_weight_lbs AS FLOAT) + (CAST(b.birth_weight_oz AS FLOAT)/16)) AS n_value,
+			'lbs' AS units,
+			'The State' AS downloaded_from,
+			b.imported_at AS downloaded_at
+		FROM vital_records.birth b
+		JOIN private.identifiers i
+			ON i.source_id = b.state_file_number
+			AND i.source_column = 'state_file_number'
+			AND i.source_table = 'birth'
+			AND i.source_schema = 'vital_records'
+		WHERE b.imported_to_dw = 'FALSE'
+
+
 
 	UPDATE vital_records.birth
 		SET imported_to_dw = 'TRUE'
