560e7031e3201044974e12c24b69c8583fc0375d
Jake <github@jakewendt.com>
2016-03-11 15:01:36 -0800

    Took ownership back.

--- production/create_core_database.sql
+++ production/create_core_database.sql
@@ -15,6 +15,7 @@ CREATE DATABASE chirp WITH TRUSTWORTHY ON;
 GO
 USE chirp;
 
+--Database diagram support objects cannot be installed because this database does not have a valid owner. To continue, first use the Files page of the Database Properties dialog box or the ALTER AUTHORIZATION statement to set the database owner to a valid login, then add the database diagram support objects.
 --Wanted to see these Database Diagrams and this seemed to work.
 --This changes the database owner to [sa]. I'd prefer to keep it.
 --ALTER AUTHORIZATION ON DATABASE::chirp TO [sa];
