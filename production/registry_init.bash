cd64dd4a0d440c5c3b9fcdeae00e66e55855f336
Jake <github@jakewendt.com>
2016-03-15 12:48:26 -0700

    Comments

--- production/registry_init.bash
+++ production/registry_init.bash
@@ -22,6 +22,8 @@ cat create_vital_records_procedures.sql
 #	Is 'clr enabled' a security issue?
 #	I'm turning it on only as needed. Worth it?
 #
+#	It's probably NOT a good idea to install this when we are actually in production.
+#
 echo "EXEC sp_configure 'clr enabled', 1;"
 echo "RECONFIGURE;"
 cat ../tSQLt_V1.0.5873.27393/tSQLt.class.sql
@@ -29,17 +31,29 @@ echo "EXEC sp_configure 'clr enabled', 0;"
 echo "RECONFIGURE;"
 cat testing_procedures.sql
 
+
 #	No more bulk importing of all this as most won't actually be used this way.
 #../scripts/csv_to_concept_codes.bash ../scripts/vital_records/*csv
 
 for concept in DOB Sex Height Weight Race; do
-
 	echo "INSERT INTO dbo.concepts VALUES ("
 	echo -e "\t'DEM:$concept',"
 	echo -e "\t'/Demographics/$concept',"
 	echo -e "\t'Demographics:$concept');"
-
 done
+
+#
+#  14567 ICD9DX
+#   3882 ICD9SG
+#  18449 subtotal
+#
+#  16710 HCPC
+#  69834 ICD10DX
+#      5 DEM (manually added for testing)
+#  86549 subtotal
+#
+# 104998 total concepts
+#
 
 #tail -n +2 ../all_lmrp/hcpc_code_lookup.csv | sort -r -k1,1 -k2,2n \
 #	| awk -F, '( $1 != prev ){ print; prev=$1 }' | sort -k1,1 \
