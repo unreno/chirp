556c34f3b9c1c9eec23bd580cd590cf4800590c3
Jake <github@jakewendt.com>
2016-03-18 14:39:18 -0700

    Adding first and last names to random birth data.

--- production/create_vital_records_procedures.sql
+++ production/create_vital_records_procedures.sql
@@ -14,12 +14,20 @@ BEGIN
 	WHILE @count < @total
 	BEGIN
 		SET @count = @count + 1;
+		DECLARE @sex VARCHAR(1) = dbo.random_sex()
+		DECLARE @name_first VARCHAR(255)
+		IF @sex = 'M'
+			SET @name_first = dbo.random_male_name()
+		ELSE
+			SET @name_first = dbo.random_female_name()
 		INSERT INTO vital_records.birth 
-			( birthid, state_file_number, date_of_birth, sex, birth_weight_lbs, birth_weight_oz )
+			( birthid, state_file_number, date_of_birth, sex, 
+				name_first, name_last,
+				birth_weight_lbs, birth_weight_oz )
 			VALUES 
 			( @count, CAST(RAND()*1e9 AS INT), 
-				dbo.random_date(),
-				dbo.random_sex(),
+				dbo.random_date(), @sex,
+				@name_first, dbo.random_last_name(),
 				CAST(RAND()*5 AS INT)+5,
 				CAST(RAND()*16 AS INT) 
 			);
