d55165281ed3fe5476fcda24ffd1f6f85b4dfc1a
Jake <github@jakewendt.com>
2016-03-21 11:35:59 -0700

    Added list of newborn screening tests in prep to add fake fields and data.

--- production/health_lab_structure.sql
+++ production/health_lab_structure.sql
@@ -13,6 +13,52 @@ CREATE TABLE health_lab.newborn_screening (
 	sex VARCHAR(1),
 	blahblah VARCHAR(250),
 	yadayada VARCHAR(250),
+
+
+
+--Congenital Adrenal Hyperplasia
+--Congenital Hypothyroidism
+--Sickle Cell Disease S/S
+--Sickle Cell Disease S/C
+--Thalassemia Major
+--Biotinidase Deficiency
+--Galactosemia
+--Argininosuccinate Lyase Deficiency (ASA)
+--Classic Citrullinemia
+--Citrullinemia Type II
+--Homocystinuria
+--Hyperphenylalanemia, including Phenylketonuria
+--Tyrosinemia, Type 1
+--Tyrosinemia, Type 2
+--Beta-Ketothiolase Deficiency
+--Glutaric Aciduria, Type I (Glutaryl-CoA Dehydrogenase Deficiency)
+--Isovaleryl-CoA Dehydrogenase Deficiency (Isovaleric Acidemia)
+--Maple Syrup Urine Disease
+--Methylmalonic Acidemia (MMA; 8 types)
+--Methylmalonic Aciduria, Vitamin B-12 Responsive
+--Methylmalonic Aciduria, Vitamin B-12 Nonresponsive
+--Vitamin B12 Metabolic Defect with Methylmalonicacidemia and Homocystinuria
+--Propionic Acidemia (PA)
+--3-methylglutaconyl-CoA Hydratase Deficiency
+--3-methylglutaconyl-CoA Aciduria Type I
+--3-methylglutaconyl-CoA Aciduria Type II
+--3-methylglutaconyl-CoA Aciduria Type III
+--3-methylglutaconyl-CoA aciduria Type IV
+--Multiple Carboxylase Deficiency
+--Carnitine uUptake/Transporter Defects
+--Carnitine-Acylcarnitine Translocase Deficiency
+--Carnitine Transporter Defect
+--Carnitine Palmitoyl Transferase I Deficiency (CPT I)
+--Carnitine PalmitoylTransferase II Deficiency (CPT II)
+--Glutaric Aciduria, Type II (Multiple Acyl-CoA Dehydrogenase Deficiency (MADD))
+--Very Long Chain Acyl-CoA Dehydrogenase Deficiency (VLCADD)
+--Long Chain L-3 Hydroxyacyl-CoA Dehydrogenase Deficiency (LCHADD)
+--Medium Chain Acyl-CoA Dehydrogenase Deficiency (MCADD)
+--Short Chain Acyl-CoA Dehydrogenase Deficiency (SCADD)
+--Cystic Fibrosis
+
+
+
 );
 GO
 
