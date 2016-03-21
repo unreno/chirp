e3051ea0dc0a067926c02f7268f834f719db15c1
Jake <github@jakewendt.com>
2016-03-21 14:13:10 -0700

    Importing fake fields from newborn screening to observations.

--- production/warehouse_procedures.sql
+++ production/warehouse_procedures.sql
@@ -1,8 +1,65 @@
 
-IF OBJECT_ID ( 'dbo.import_into_data_warehouse_by_table_birth', 'P' ) IS NOT NULL
-	DROP PROCEDURE dbo.import_into_data_warehouse_by_table_birth;
+IF OBJECT_ID ( 'dbo.import_into_data_warehouse_by_table_health_lab_newborn_screening', 'P' ) IS NOT NULL
+	DROP PROCEDURE dbo.import_into_data_warehouse_by_table_health_lab_newborn_screening;
 GO
-CREATE PROCEDURE dbo.import_into_data_warehouse_by_table_birth
+CREATE PROCEDURE dbo.import_into_data_warehouse_by_table_health_lab_newborn_screening
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	INSERT INTO dbo.observations
+		(chirp_id, provider_id, location_id, started_at, value_type, 
+			concept, s_value, downloaded_from, downloaded_at) 
+		SELECT chirp_id, provider_id, location_id, started_at, value_type, 
+			concept, s_value, downloaded_from, downloaded_at
+		FROM (
+			SELECT i.chirp_id, n.date_of_birth AS started_at, 
+				'789' AS provider_id,
+				'123' AS location_id,
+				'S' AS value_type,
+
+
+				'DEM:Other' AS concept,
+				yadayada, blahblah,
+
+
+				'Health Lab' AS downloaded_from,
+				n.imported_at AS downloaded_at
+			FROM health_lab.newborn_screening n
+			JOIN private.identifiers i
+				ON    i.source_id     = n.id 
+					AND i.source_column = 'id'
+					AND i.source_table  = 'newborn_screening'
+					AND i.source_schema = 'health_lab'
+			WHERE n.imported_to_dw = 'FALSE'
+		) arbitraryrequiredandignoredname
+		UNPIVOT (
+
+
+			s_value FOR ignoreconcept IN ( yadayada, blahblah )
+
+
+		) AS anotherarbitraryrequiredname
+
+	UPDATE n
+		SET imported_to_dw = 'TRUE'
+		FROM health_lab.newborn_screening n
+		JOIN private.identifiers i
+			ON  i.source_id     = n.id 
+			AND i.source_column = 'id'
+			AND i.source_table  = 'newborn_screening'
+			AND i.source_schema = 'health_lab'
+		WHERE imported_to_dw = 'FALSE'
+			AND i.id IS NOT NULL    
+
+END	--	CREATE PROCEDURE dbo.import_into_data_warehouse_by_table_health_lab_newborn_screening
+GO
+
+
+IF OBJECT_ID ( 'dbo.import_into_data_warehouse_by_table_vital_records_birth', 'P' ) IS NOT NULL
+	DROP PROCEDURE dbo.import_into_data_warehouse_by_table_vital_records_birth;
+GO
+CREATE PROCEDURE dbo.import_into_data_warehouse_by_table_vital_records_birth
 AS
 BEGIN
 	SET NOCOUNT ON;
@@ -16,7 +73,7 @@ BEGIN
 			SELECT i.chirp_id, b.date_of_birth AS started_at, 
 				'123' AS provider_id,
 				'456' AS location_id,
-				'S' as value_type,
+				'S' AS value_type,
 				CONVERT(VARCHAR(255), b.sex) AS [DEM:Sex],
 				'The State' AS downloaded_from,
 				b.imported_at AS downloaded_at
@@ -41,7 +98,7 @@ BEGIN
 			SELECT i.chirp_id, b.date_of_birth AS started_at, 
 				'123' AS provider_id,
 				'456' AS location_id,
-				'T' as value_type,
+				'T' AS value_type,
 				b.date_of_birth AS [DEM:DOB],
 				'The State' AS downloaded_from,
 				b.imported_at AS downloaded_at
@@ -66,7 +123,7 @@ BEGIN
 --			SELECT i.chirp_id, b.date_of_birth AS started_at, 
 --				'123' AS provider_id,
 --				'456' AS location_id,
---				'N' as value_type,
+--				'N' AS value_type,
 --				(CAST(b.birth_weight_lbs AS FLOAT) + (CAST(b.birth_weight_oz AS FLOAT)/16)) AS [DEM:Weight],
 --				'The State' AS downloaded_from,
 --				b.imported_at AS downloaded_at
@@ -93,7 +150,7 @@ BEGIN
 			'123' AS provider_id,
 			'456' AS location_id,
 			b.date_of_birth AS started_at, 
-			'N' as value_type,
+			'N' AS value_type,
 			'DEM:Weight' AS concept,
 			(CAST(b.birth_weight_lbs AS FLOAT) + (CAST(b.birth_weight_oz AS FLOAT)/16)) AS n_value,
 			'lbs' AS units,
@@ -109,15 +166,23 @@ BEGIN
 			AND b.birth_weight_lbs IS NOT NULL
 			AND b.birth_weight_oz IS NOT NULL
 
+--	UPDATE vital_records.birth
+--		SET imported_to_dw = 'TRUE'
+--		WHERE imported_to_dw = 'FALSE'
 
-
-	UPDATE vital_records.birth
+	UPDATE n
 		SET imported_to_dw = 'TRUE'
+		FROM vital_records.birth n
+		JOIN private.identifiers i
+			ON  i.source_id     = n.state_file_number
+			AND i.source_column = 'state_file_number'
+			AND i.source_table  = 'birth'
+			AND i.source_schema = 'vital_records'
 		WHERE imported_to_dw = 'FALSE'
+			AND i.id IS NOT NULL    
 
-END
+END -- CREATE PROCEDURE dbo.import_into_data_warehouse_by_table_vital_records_birth
 GO
-
 
 
 IF OBJECT_ID ( 'dbo.import_into_data_warehouse_by_schema', 'P' ) IS NOT NULL
@@ -128,8 +193,8 @@ AS
 BEGIN
 	SET NOCOUNT ON;
 
-	DECLARE @table VARCHAR(50)
-	DECLARE @proc VARCHAR(50)
+	DECLARE @table VARCHAR(250)
+	DECLARE @proc VARCHAR(250)
 
 	DECLARE tables CURSOR FOR SELECT t.name 
 		FROM sys.tables AS t
@@ -141,16 +206,16 @@ BEGIN
 	WHILE(1=1)BEGIN
 		FETCH tables INTO @table;
 		IF(@@FETCH_STATUS <> 0) BREAK
-		--	EXEC dbo.import_into_data_warehouse_by_table(@schema,@table)
-		SET @proc = 'dbo.import_into_data_warehouse_by_table_' + @table
+
+		SET @proc = 'dbo.import_into_data_warehouse_by_table_' + @schema + '_' + @table
 
 		IF OBJECT_ID ( @proc, 'P' ) IS NOT NULL
 		BEGIN
-			PRINT 'Importing select fields from ' + @table
+			PRINT 'Importing select fields from ' + @schema + '.' + @table
 			EXEC @proc
 		END
 		ELSE
-			PRINT 'Ignoring table ' + @table
+			PRINT 'Ignoring table ' + @schema + '.' + @table
 
 	END
 	CLOSE tables;
@@ -183,6 +248,7 @@ BEGIN
 
 		--Until there are more than a dozen, this above is a bit excessive!
 		EXEC dbo.import_into_data_warehouse_by_schema 'vital_records'
+		EXEC dbo.import_into_data_warehouse_by_schema 'health_lab'
 
 --	END
 --	CLOSE schemas;
