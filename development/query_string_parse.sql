cc863600e173a6506af542a1f77f0821a4140a81
Jake <github@jakewendt.com>
2016-03-22 13:50:49 -0700

    Practicing string parsing

--- development/query_string_parse.sql
+++ development/query_string_parse.sql
@@ -2,7 +2,7 @@
 --There is no error checking here so the query string 
 --needs to be perfect to work.
 -------------------------------1---a-------------b----------c
-DECLARE @query VARCHAR(250) = 'http://example.com/over/there?name=ferret&color=brown&size=7&'
+DECLARE @query VARCHAR(250) = 'http://example.com/over/there?name=ferret&color=brown&size=7&truncated'
 PRINT @query
 
 DECLARE @a INT = CharIndex('://',@query)
@@ -23,13 +23,23 @@ PRINT @path
 DECLARE @params VARCHAR(250) = SUBSTRING(@query, @c+1, LEN(@query)-@c)
 PRINT @params
 
+DECLARE @kvpairs TABLE ( k VARCHAR(250), v VARCHAR(250) )
 WHILE(LEN(@params)>0)BEGIN
 	DECLARE @eql INT = CharIndex('=',@params)
-	DECLARE @amp INT = CharIndex('&',@params)
+	IF @eql = 0 SET @eql = LEN(@params)+1
 	DECLARE @k VARCHAR(250) = SUBSTRING(@params,1,@eql-1)
+
+	INSERT INTO @kvpairs (k)VALUES(@k)
+
+	DECLARE @amp INT = CharIndex('&',@params,@eql)
+	IF @amp = 0 BREAK
 	DECLARE @v VARCHAR(250) = SUBSTRING(@params,@eql+1,@amp-@eql-1)
 	--PRINT @params
-	PRINT @k + ' = ' + @v
-	SET @params = SUBSTRING(@params,CharIndex('&',@params)+1, LEN(@params))
+	UPDATE @kvpairs SET v = @v WHERE k = @k
+	SET @params = SUBSTRING(@params, @amp+1, LEN(@params))
 END
+
+SELECT * FROM @kvpairs
+
+
 
