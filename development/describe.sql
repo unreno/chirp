19d9af57e42eacce0856a6f586b290df7010ddd1
Jake <github@jakewendt.com>
2016-04-14 10:37:36 -0700

    Created a 'describe' procedure

---
+++ development/describe.sql
@@ -0,0 +1,23 @@
+
+
+
+IF OBJECT_ID ( 'dbo.describe', 'P' ) IS NOT NULL
+	DROP PROCEDURE dbo.describe;
+GO
+CREATE PROCEDURE dbo.describe( @schema_and_table VARCHAR(255) )
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT c.name, t.name, c.max_length
+		FROM sys.columns c
+		JOIN sys.types t ON t.system_type_id = c.system_type_id
+		WHERE [object_id] = OBJECT_ID( @schema_and_table );
+  
+END
+GO
+
+
+-- EXEC dbo.describe 'dbo.observations'
+
+
