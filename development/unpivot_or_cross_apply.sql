3eba2f2a6ef32641d1b10d33d9073e7fce11300b
Jake <github@jakewendt.com>
2016-04-15 09:26:50 -0700

    Created little demo to compare unpivot and cross apply values.

--- development/unpivot_or_cross_apply.sql
+++ development/unpivot_or_cross_apply.sql
@@ -4,3 +4,27 @@
 -- I think that CROSS APPLY would return NULL for missing
 -- values which UNPIVOT would ignore them completely.
 -- I need to create a little demo to confirm.
+
+
+DECLARE @t TABLE( id INT, a VARCHAR(255), b VARCHAR(255) )
+INSERT INTO @t VALUES (1,'apple','banana'), (2,'orange',NULL)
+SELECT * FROM @t
+
+--CROSS APPLY will includes NULL values
+SELECT id, fruit FROM @t
+CROSS APPLY ( VALUES
+	( a ), ( b ) ) v (fruit)
+
+--While UNPIVOT will ignore
+SELECT id, fruit FROM @t
+UNPIVOT (
+	fruit FOR ignore IN ( a, b )
+) v
+
+--Of course, if this is being wrapped in another SELECT or INSERT
+--can simply add a WHERE to check that the results aren't NULL
+SELECT id, fruit FROM @t
+CROSS APPLY ( VALUES
+	( a ), ( b ) ) v (fruit)
+WHERE fruit IS NOT NULL
+
