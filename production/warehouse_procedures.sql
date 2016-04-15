f44e226b6d32720a7ba01a627527783a3193c23f
Jake <github@jakewendt.com>
2016-04-15 12:35:41 -0700

    Cleanup

--- production/warehouse_procedures.sql
+++ production/warehouse_procedures.sql
@@ -11,30 +11,21 @@ BEGIN
 	INSERT INTO dbo.observations
 		(chirp_id, provider_id, started_at,
 			concept, value, units, downloaded_from, downloaded_at)
---		SELECT chirp_id, provider_id, started_at,
---			concept, value, downloaded_from, downloaded_at
---		FROM (
---	Don't think that I need the AS's
---	Don't need to UNPIVOT as is already UNPIVOTed
-			SELECT i.chirp_id, 
-				8675309 AS provider_id,
-				n.service_at AS started_at,
-				code AS concept,
-				value AS value,
-				units AS units,
-				'Fake Doctor 1' AS downloaded_from,
-				n.imported_at AS downloaded_at
-			FROM fakedoc1.emrs n
-			JOIN private.identifiers i
-				ON    i.source_id     = n.record_number
-					AND i.source_column = 'record_number'
-					AND i.source_table  = 'emrs'
-					AND i.source_schema = 'fakedoc1'
-			WHERE n.imported_to_dw = 'FALSE'
---		) arbitraryrequiredandignoredname
---		UNPIVOT (
---			value FOR concept IN ( [DEM:Weight], [DEM:Height] )
---		) AS anotherarbitraryrequiredname
+		SELECT i.chirp_id, 
+			8675309 AS provider_id,
+			n.service_at AS started_at,
+			code AS concept,
+			value AS value,
+			units AS units,
+			'Fake Doctor 1' AS downloaded_from,
+			n.imported_at AS downloaded_at
+		FROM fakedoc1.emrs n
+		JOIN private.identifiers i
+			ON    i.source_id     = n.record_number
+				AND i.source_column = 'record_number'
+				AND i.source_table  = 'emrs'
+				AND i.source_schema = 'fakedoc1'
+		WHERE n.imported_to_dw = 'FALSE'
 
 	UPDATE n
 		SET imported_to_dw = 'TRUE'
@@ -51,8 +42,6 @@ END	--	CREATE PROCEDURE dbo.import_into_data_warehouse_by_table_fakedoc1_emrs
 GO
 
 
-
-
 IF OBJECT_ID ( 'dbo.import_into_data_warehouse_by_table_health_lab_newborn_screening', 'P' ) IS NOT NULL
 	DROP PROCEDURE dbo.import_into_data_warehouse_by_table_health_lab_newborn_screening;
 GO
@@ -62,25 +51,14 @@ BEGIN
 	SET NOCOUNT ON;
 
 	INSERT INTO dbo.observations
---		(chirp_id, provider_id, location_id, started_at, value_type,
---			concept, s_value, downloaded_from, downloaded_at)
 		(chirp_id, provider_id, started_at,
-			concept, value, downloaded_from, downloaded_at)
---		SELECT chirp_id, provider_id, location_id, started_at, value_type,
---			concept, s_value, downloaded_from, downloaded_at
+			concept, value, units, downloaded_from, downloaded_at)
 		SELECT chirp_id, provider_id, started_at,
-			concept, value, downloaded_from, downloaded_at
+			cav.concept, cav.value, cav.units, downloaded_from, downloaded_at
 		FROM (
 			SELECT i.chirp_id, n.date_of_birth AS started_at,
 				'789' AS provider_id,
---				'123' AS location_id,
---				'S' AS value_type,
-
-
-				'DEM:Other' AS concept,
 				testresults1, testresults2, testresults3, testresults4, testresults5,
-
-
 				'Health Lab' AS downloaded_from,
 				n.imported_at AS downloaded_at
 			FROM health_lab.newborn_screening n
@@ -91,24 +69,38 @@ BEGIN
 					AND i.source_schema = 'health_lab'
 			WHERE n.imported_to_dw = 'FALSE'
 		) arbitraryrequiredandignoredname
-		UNPIVOT (
+		CROSS APPLY ( VALUES
+			( 'TR1', testresults1, 'cm' ),
+			( 'TR2', testresults2, 'in' ),
+			( 'TR3', testresults3, 'Degrees' ),
+			( 'TR4', testresults4, 'mm Hg' ),
+			( 'TR5', testresults5, '%' )
+		) cav ( concept, value, units )
+		WHERE cav.value IS NOT NULL
 
-
---			s_value FOR ignoreconcept IN ( testresults1, testresults2 )
-			value FOR ignoreconcept IN ( testresults1, testresults2, testresults3, testresults4, testresults5 )
-
-
-		) AS anotherarbitraryrequiredname
-
---	CROSS APPLY 
---	WHERE value IS NOT NULL
-
-
-
-
-
-
-
+--	INSERT INTO dbo.observations
+--		(chirp_id, provider_id, started_at,
+--			concept, value, downloaded_from, downloaded_at)
+--		SELECT chirp_id, provider_id, started_at,
+--			concept, value, downloaded_from, downloaded_at
+--		FROM (
+--			SELECT i.chirp_id, n.date_of_birth AS started_at,
+--				'789' AS provider_id,
+--				'DEM:Other' AS concept,
+--				testresults1, testresults2, testresults3, testresults4, testresults5,
+--				'Health Lab' AS downloaded_from,
+--				n.imported_at AS downloaded_at
+--			FROM health_lab.newborn_screening n
+--			JOIN private.identifiers i
+--				ON    i.source_id     = n.id
+--					AND i.source_column = 'id'
+--					AND i.source_table  = 'newborn_screening'
+--					AND i.source_schema = 'health_lab'
+--			WHERE n.imported_to_dw = 'FALSE'
+--		) arbitraryrequiredandignoredname
+--		UNPIVOT (
+--			value FOR ignoreconcept IN ( testresults1, testresults2, testresults3, testresults4, testresults5 )
+--		) AS anotherarbitraryrequiredname
 
 	UPDATE n
 		SET imported_to_dw = 'TRUE'
@@ -134,21 +126,13 @@ BEGIN
 	SET NOCOUNT ON;
 
 	INSERT INTO dbo.observations
---		(chirp_id, provider_id, location_id, started_at, value_type,
---			concept, s_value, downloaded_from, downloaded_at)
 		(chirp_id, provider_id, started_at,
 			concept, value, downloaded_from, downloaded_at)
---		SELECT chirp_id, provider_id, location_id, started_at, value_type,
---			concept, s_value, downloaded_from, downloaded_at
 		SELECT chirp_id, provider_id, started_at,
 			concept, value, downloaded_from, downloaded_at
 		FROM (
 			SELECT i.chirp_id, b.date_of_birth AS started_at,
 				'123' AS provider_id,
---				'456' AS location_id,
---				'S' AS value_type,
---				CONVERT(VARCHAR(255), b.sex) AS [DEM:Sex],	--Why CONVERT and not CAST?
---	CONVERT is SQL Server, CAST is ANSI, so I choose CAST
 				CAST(b.sex AS VARCHAR(255)) AS [DEM:Sex],
 				CAST(b.apgar_1 AS VARCHAR(255)) AS [APGAR1],
 				CAST(b.apgar_5 AS VARCHAR(255)) AS [APGAR5],
@@ -164,29 +148,18 @@ BEGIN
 			WHERE b.imported_to_dw = 'FALSE'
 		) arbitraryrequiredandignoredname
 		UNPIVOT (
---			s_value FOR concept IN ( [DEM:Sex] )
 			value FOR concept IN ( [DEM:Sex], [APGAR1], [APGAR5], [APGAR10] )
 		) AS anotherarbitraryrequiredname
 
 	INSERT INTO dbo.observations
---		(chirp_id, provider_id, location_id, started_at, value_type,
---			concept, t_value, downloaded_from, downloaded_at)
 		(chirp_id, provider_id, started_at,
 			concept, value, downloaded_from, downloaded_at)
---		SELECT chirp_id, provider_id, location_id, started_at, value_type,
---			concept, t_value, downloaded_from, downloaded_at
 		SELECT chirp_id, provider_id, started_at,
 			concept, value, downloaded_from, downloaded_at
 		FROM (
 			SELECT i.chirp_id, b.date_of_birth AS started_at,
 				'123' AS provider_id,
---				'456' AS location_id,
-
---				'T' AS value_type,
---				b.date_of_birth AS [DEM:DOB],
---	If we drop the specific value types
 				CAST(b.date_of_birth AS VARCHAR(255)) AS [DEM:DOB],
-
 				'The State' AS downloaded_from,
 				b.imported_at AS downloaded_at
 			FROM vital_records.birth b
@@ -198,58 +171,21 @@ BEGIN
 			WHERE b.imported_to_dw = 'FALSE'
 		) arbitraryrequiredandignoredname
 		UNPIVOT (
---			t_value FOR concept IN ( [DEM:DOB] )
 			value FOR concept IN ( [DEM:DOB] )
 		) AS anotherarbitraryrequiredname
-
---	INSERT INTO dbo.observations
---		(chirp_id, provider_id, location_id, started_at, value_type,
---			concept, n_value, downloaded_from, downloaded_at)
---		SELECT chirp_id, provider_id, location_id, started_at, value_type,
---			concept, n_value, downloaded_from, downloaded_at
---		FROM (
---			SELECT i.chirp_id, b.date_of_birth AS started_at,
---				'123' AS provider_id,
---				'456' AS location_id,
---				'N' AS value_type,
---				(CAST(b.birth_weight_lbs AS FLOAT) + (CAST(b.birth_weight_oz AS FLOAT)/16)) AS [DEM:Weight],
---				'The State' AS downloaded_from,
---				b.imported_at AS downloaded_at
---			FROM vital_records.birth b
---			JOIN private.identifiers i
---				ON i.source_id = b.state_file_number
---				AND i.source_column = 'state_file_number'
---				AND i.source_table = 'birth'
---				AND i.source_schema = 'vital_records'
---			WHERE b.imported_to_dw = 'FALSE'
---		) arbitraryrequiredandignoredname
---		UNPIVOT (
---			n_value FOR concept IN ( [DEM:Weight] )
---		) AS anotherarbitraryrequiredname
-
-
---	The UNPIVOT seems really unnecessary if only doing 1 column.
---	Particularly when it is unique (contains units)
 
 -- ORDER MATTERS! SELECT ORDER MUST BE THE SAME AS INSERT ORDER!
 
 	INSERT INTO dbo.observations
---		(chirp_id, provider_id, location_id, started_at, value_type,
---			concept, n_value, units, downloaded_from, downloaded_at)
 		(chirp_id, provider_id, started_at,
 			concept, value, units, downloaded_from, downloaded_at)
 		SELECT i.chirp_id,
 			'123' AS provider_id,
---			'456' AS location_id,
 			b.date_of_birth AS started_at,
-
---			'N' AS value_type,
 			'DEM:Weight' AS concept,
---			(CAST(b.birth_weight_lbs AS FLOAT) + (CAST(b.birth_weight_oz AS FLOAT)/16)) AS n_value,
---	If we drop the specific value types
-			CAST((CAST(b.birth_weight_lbs AS FLOAT) + (CAST(b.birth_weight_oz AS FLOAT)/16)) AS VARCHAR(255)) AS value,
-
-
+			CAST(
+				(CAST(b.birth_weight_lbs AS FLOAT) + (CAST(b.birth_weight_oz AS FLOAT)/16)) AS VARCHAR(255)
+			) AS value,
 			'lbs' AS units,
 			'The State' AS downloaded_from,
 			b.imported_at AS downloaded_at
@@ -262,10 +198,6 @@ BEGIN
 		WHERE b.imported_to_dw = 'FALSE'
 			AND b.birth_weight_lbs IS NOT NULL
 			AND b.birth_weight_oz IS NOT NULL
-
---	UPDATE vital_records.birth
---		SET imported_to_dw = 'TRUE'
---		WHERE imported_to_dw = 'FALSE'
 
 	UPDATE n
 		SET imported_to_dw = 'TRUE'
