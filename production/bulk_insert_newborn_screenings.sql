894d97187b23440983504ed9ada4ce3f514e8fa5
Jake <github@jakewendt.com>
2016-05-13 14:43:08 -0700

    Added notes.

--- production/bulk_insert_newborn_screenings.sql
+++ production/bulk_insert_newborn_screenings.sql
@@ -1,4 +1,11 @@
 
+--
+-- I had to convert these malformed csv files into malformed psv files
+-- to get bulk import to correctly import the contents.
+-- awk -f /cygdrive/z/Renown\ Project/CHIRP/Personal\ folders/Jake/chirp/scripts/csv_prepare_for_bulk_insert.awk September\ 2015.csv > September\ 2015.csv.psv
+--
+
+
 DECLARE @bulk_cmd VARCHAR(1000);
 
 SET @bulk_cmd = 'BULK INSERT health_lab.bulk_insert_newborn_screenings_2015
