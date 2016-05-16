540ee701e720b0d73b407974ebd601b06f03790e
Jake <github@jakewendt.com>
2016-05-16 10:31:31 -0700

    Added occ,occup,business,and industry decoders.

--- production/registry_init.bash
+++ production/registry_init.bash
@@ -4,7 +4,7 @@
 #	and as this will really only be used once,
 #	just catting them.
 
-DB_NAME='chirp_dev4'
+DB_NAME='chirp'
 
 #	Put this here so can programmatically select dev or prod
 echo "USE master;"
