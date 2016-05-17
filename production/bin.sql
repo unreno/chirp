bd86080d2ddcfd9bf23544c02f7eb50de0f15082
Jake <github@jakewendt.com>
2016-05-16 19:56:43 -0700

    Only add imported_at and imported_to_dw columns if don't exist.

--- production/bin.sql
+++ production/bin.sql
@@ -47,13 +47,21 @@ BEGIN
 		'ALTER TABLE ' + @schema + '.[' + @table + '] DROP COLUMN imported_at;'
 	PRINT @cmd
 	EXEC (@cmd);	--	Parenthese required here!
-*/
 
 	--Add column with constraint
 	SELECT @cmd = 'ALTER TABLE [' + @schema + '].[' + @table + 
 		'] ADD imported_at DATETIME CONSTRAINT '
 		+ @cname + ' DEFAULT CURRENT_TIMESTAMP NOT NULL ;';
---	PRINT @cmd
+	PRINT @cmd
+	EXEC (@cmd);	--	Parenthese required here!
+*/
+
+	--Add column with constraint if doesn't exist
+	SELECT @cmd = 'IF COL_LENGTH(''[' + @schema + '].[' + @table + 
+		']'',''imported_at'') IS NULL '+
+		'ALTER TABLE [' + @schema + '].[' + @table + 
+		'] ADD imported_at DATETIME CONSTRAINT '
+		+ @cname + ' DEFAULT CURRENT_TIMESTAMP NOT NULL ;';
 	EXEC (@cmd);	--	Parenthese required here!
 
 END
@@ -115,12 +123,21 @@ BEGIN
 		'ALTER TABLE ' + @schema + '.[' + @table + '] DROP COLUMN imported_to_dw;'
 	PRINT @cmd
 	EXEC (@cmd);	--	Parenthese required here!
-*/
+
 	--Add column with constraint
 	SELECT @cmd = 'ALTER TABLE [' + @schema + '].[' + @table + 
 		'] ADD imported_to_dw BIT CONSTRAINT '
 		+ @cname + ' DEFAULT ''FALSE'' NOT NULL ;';
 --	PRINT @cmd
+	EXEC (@cmd);	--	Parenthese required here!
+*/
+
+	--Add column with constraint if doesn't exist
+	SELECT @cmd = 'IF COL_LENGTH(''[' + @schema + '].[' + @table + 
+		']'',''imported_to_dw'') IS NULL '+
+		'ALTER TABLE [' + @schema + '].[' + @table + 
+		'] ADD imported_to_dw BIT CONSTRAINT '
+		+ @cname + ' DEFAULT ''FALSE'' NOT NULL ;';
 	EXEC (@cmd);	--	Parenthese required here!
 
 END
