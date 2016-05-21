35eb9911143d5d067f9d2cbb405776efb6f05e10
Jake <github@jakewendt.com>
2016-05-20 21:46:30 -0700

    Better dictionary extraction.

--- production/data_dictionary_extraction.sql
+++ production/data_dictionary_extraction.sql
@@ -16,6 +16,23 @@ SELECT @SQL = (
 );
 EXECUTE sp_executesql @SQL;
 
+SELECT @SQL = (
+	SELECT 'INSERT INTO #group_counts (field,group_count) ' +
+		'SELECT ''' + name + ''' AS field, ' +
+		'COUNT(*) AS group_count ' +
+		'FROM vital.births WHERE ' + name + ' IS NULL OR CAST(' + name + ' AS VARCHAR) = '''';'
+	FROM sys.columns
+	WHERE object_id = OBJECT_ID('vital.births')
+	FOR XML PATH ('')
+);
+EXECUTE sp_executesql @SQL;
+
+IF OBJECT_ID('tempdb..#dict_counts', 'U') IS NOT NULL
+DROP TABLE #dict_counts
+CREATE TABLE #dict_counts( field VARCHAR(255), label VARCHAR(255),definition VARCHAR(255),
+	codeset VARCHAR(255), code INT, value VARCHAR(255), group_count INT )
+
+INSERT INTO #dict_counts
 SELECT d.field, d.label, d.definition, d.codeset, c.code, c.value, g.group_count
 FROM dbo.dictionary d
 LEFT JOIN dbo.codes c
@@ -25,4 +42,16 @@ ON g.field = d.field AND g.value = CAST(c.code AS VARCHAR(255))
 WHERE d._table = 'births'
 ORDER BY d.field, CAST(c.code AS INT)
 
+UPDATE #dict_counts
+SET group_count = 0 
+WHERE group_count IS NULL and codeset IS NOT NULL
+
+UPDATE #dict_counts
+SET group_count = g.group_count
+FROM #dict_counts d
+INNER JOIN #group_counts g ON ( g.field = d.field AND g.value IS NULL )
+WHERE d.group_count IS NULL and d.codeset IS NULL
+
+SELECT * FROM #dict_counts
+ORDER BY field, code
 
