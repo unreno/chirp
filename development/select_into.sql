14d89106a6d6d1e4ecee43358efb6eb8e90988c6
Jake <github@jakewendt.com>
2016-05-23 16:56:30 -0700

    Adding another dev snippet.

---
+++ development/select_into.sql
@@ -0,0 +1,8 @@
+
+
+-- SELECT * INTO tablename FROM someothertable
+-- is for creating a table
+
+-- Use INSERT INTO tablename SELECT * FROM someothertable
+-- when said target table already exists.
+
