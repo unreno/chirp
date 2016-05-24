53773e039d10dc781ab1f4b4fd02f6995e520e5a
Jake <github@jakewendt.com>
2016-05-23 17:00:01 -0700

    Adding another dev snippet.

--- development/select_into.sql
+++ development/select_into.sql
@@ -3,6 +3,9 @@
 -- SELECT * INTO tablename FROM someothertable
 -- is for creating a table
 
+-- This is actually nice for copying the structure of a table. 
+-- Just add a false condition like WHERE 1 = 0 so it won't copy any data.
+
 -- Use INSERT INTO tablename SELECT * FROM someothertable
 -- when said target table already exists.
 
