f1b3c5faa194d0a3030c14853c32b5e5bfca1791
Jake <github@jakewendt.com>
2016-06-13 09:33:48 -0700

    testing github syntax highlighting.

--- production/github_syntax_highlighting_test.sql
+++ production/github_syntax_highlighting_test.sql
@@ -1,11 +1,9 @@
-
 CREATE PROCEDURE bin.testing
 AS
 BEGIN
 	SET NOCOUNT ON;
 
 	DECLARE @rf VARCHAR(255) = REVERSE( @file_with_path )
-	-- Using CHAR(92) insteed of a \ which mucks up syntax highlighting as it "escapes" the closing quote
 	DECLARE @filename VARCHAR(255) = REVERSE( SUBSTRING( @rf, 1, ISNULL(NULLIF(CHARINDEX(CHAR(92), @rf )-1,-1),LEN(@rf))))
 	--DECLARE @filename VARCHAR(255) = REVERSE( SUBSTRING( @rf, 1, ISNULL(NULLIF(CHARINDEX('\', @rf )-1,-1),LEN(@rf))))
 	-- '	The previous line mucks up syntax highlighting by escaping the quote, so added one here.
