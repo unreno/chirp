cc4f3a19c78d4991ddc96a25d9cd475ac46fb8bb
Jake <github@jakewendt.com>
2016-06-13 10:07:18 -0700

    testing github syntax highlighting.

--- production/archive/github_syntax_highlighting_test_OK.sql
+++ production/archive/github_syntax_highlighting_test_OK.sql
@@ -5,7 +5,7 @@ BEGIN
 
 	-- CHAR of
 	-- If I upcase the word char above, highlihting fails?
-	-- '  The previous mucks up syntax highlighting by escaping the quote, so added one here.
+	-- '  The previous line mucks up syntax highlighting by escaping the quote, so added one here.
 	-- If I put the word "line" above, highlighting fails?
 
 END	--	bin.testing
