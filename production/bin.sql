384f95eab0491b4ced389b590ffdfdf4922f8495
Jake <github@jakewendt.com>
2016-06-13 11:12:01 -0700

    testing github syntax highlighting.

--- production/bin.sql
+++ production/bin.sql
@@ -13,8 +13,11 @@ GO
 /*
 --Database diagram support objects cannot be installed because this database does not have a valid owner. To continue, first use the Files page of the Database Properties dialog box or the ALTER AUTHORIZATION statement to set the database owner to a valid login, then add the database diagram support objects.
 --Wanted to see these Database Diagrams and this seemed to work.
---This changes the database owner to [sa]. I'd would prefer to keep it.
+--This changes the database owner to [sa]. I'd prefer to keep it.
 --ALTER AUTHORIZATION ON DATABASE::chirp TO [sa];
+
+-- It is really hard to believe, but sometimes apostrophes inside -- comments
+-- really mess up github's syntax highlighter parser. Wrapping -- comments in asterisk-slash chars helps.
 */
 
 
