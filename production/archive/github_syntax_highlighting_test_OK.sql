ab90c1268c15a0fbb2328566e027c133523256a9
Jake <github@jakewendt.com>
2016-06-13 10:01:04 -0700

    testing github syntax highlighting.

--- production/archive/github_syntax_highlighting_test_OK.sql
+++ production/archive/github_syntax_highlighting_test_OK.sql
@@ -7,7 +7,6 @@ BEGIN
 
 	DECLARE @filename VARCHAR(255) = REVERSE( SUBSTRING( @rf, 1, 
 		ISNULL(NULLIF(CHARINDEX(CHAR(92), @rf )-1,-1),LEN(@rf))))
-	--	ISNULL(NULLIF(CHARINDEX('\', @rf )-1,-1),LEN(@rf))))
 	-- '  The previous line mucks up syntax highlighting by escaping the quote, so added one here.
 
 END	--	bin.testing
