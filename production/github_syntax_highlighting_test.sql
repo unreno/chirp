c9a9a60934959d3abf5809a341a815aa7d62d240
Jake <github@jakewendt.com>
2016-06-13 09:35:49 -0700

    testing github syntax highlighting.

--- production/github_syntax_highlighting_test.sql
+++ production/github_syntax_highlighting_test.sql
@@ -3,7 +3,7 @@ AS
 BEGIN
 	SET NOCOUNT ON;
 
-	-- Using CHAR(92) insteed of a "\" which mucks up syntax highlighting as it "escapes" the closing quote
+	-- Using CHAR(92) insteed of a  which mucks up syntax highlighting as it "escapes" the closing quote
 
 END	--	bin.testing
 GO
