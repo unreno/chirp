1157dddb734c7eee75c9c1893b413c406e665ad2
Jake <github@jakewendt.com>
2016-06-13 10:05:55 -0700

    testing github syntax highlighting.

--- production/archive/github_syntax_highlighting_test_OK.sql
+++ production/archive/github_syntax_highlighting_test_OK.sql
@@ -5,6 +5,7 @@ BEGIN
 
 	-- Char of
 	-- '  The previous mucks up syntax highlighting by escaping the quote, so added one here.
+	-- If I put the word "line" above, highlighting fails.
 
 END	--	bin.testing
 GO
