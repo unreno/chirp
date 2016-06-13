1e90b28694f2472fae675487b1e684c333f96648
Jake <github@jakewendt.com>
2016-06-13 10:06:51 -0700

    testing github syntax highlighting.

--- production/archive/github_syntax_highlighting_test_OK.sql
+++ production/archive/github_syntax_highlighting_test_OK.sql
@@ -4,8 +4,9 @@ BEGIN
 	SET NOCOUNT ON;
 
 	-- Char of
+	-- If I upcase the word char above, highlihting fails?
 	-- '  The previous mucks up syntax highlighting by escaping the quote, so added one here.
-	-- If I put the word "line" above, highlighting fails.
+	-- If I put the word "line" above, highlighting fails?
 
 END	--	bin.testing
 GO
