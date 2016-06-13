e3f98bc220777999cdbd677c5f0fe5e2381dc2d7
Jake <github@jakewendt.com>
2016-06-13 11:08:40 -0700

    testing github syntax highlighting.

--- production/bin.sql
+++ production/bin.sql
@@ -1099,9 +1099,14 @@ AS
 BEGIN
 	SET NOCOUNT ON;
 
-	DECLARE @rf VARCHAR(255) = REVERSE( @file_with_path );
-	DECLARE @filename VARCHAR(255) = REVERSE( SUBSTRING( @rf, 1, 
-		ISNULL(NULLIF(CHARINDEX(CHAR(92), @rf )-1,-1),LEN(@rf))));
+-- Something in the following section of code mucks up github syntax highlighting.
+DECLARE @rf VARCHAR(255) = REVERSE( @file_with_path );
+DECLARE @filename VARCHAR(255) = REVERSE( SUBSTRING( @rf, 1,
+ISNULL(NULLIF(CHARINDEX(CHAR(92), @rf )-1,-1),LEN(@rf))));
+-- Using CHAR(92) instead of a \ which mucks up syntax highlighting as it "escapes" the closing quote
+-- DECLARE @filename VARCHAR(255) = REVERSE( SUBSTRING( @rf, 1, 
+--  ISNULL(NULLIF(CHARINDEX('\', @rf )-1,-1),LEN(@rf))))
+-- '  The previous line mucks up syntax highlighting by escaping the quote, so added one here.
 
 	DECLARE @bulk_cmd VARCHAR(1000) = 'BULK INSERT health_lab.bulk_insert_newborn_screenings_2016 ' +
 		'FROM ''' + @file_with_path + ''' WITH ( ' +
