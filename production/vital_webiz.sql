935e28132f7d141467f941ab5c384253ab516f5e
Jake <github@jakewendt.com>
2016-08-17 10:18:40 -0700

    Redo webiz table

--- production/vital_webiz.sql
+++ production/vital_webiz.sql
@@ -11,44 +11,30 @@ CREATE TABLE vital.webizs (
 	id INT IDENTITY(1,1),		--	1
 	CONSTRAINT vital_webizs_id PRIMARY KEY CLUSTERED (id ASC),
 
-	patient_id	INT,	-- identity NOT NULL,
-	first_name	VARCHAR(25),	-- NOT NULL,
-	middle_name	VARCHAR(25),	-- NULL,
-	last_name	VARCHAR(30),	-- NOT NULL,
-	dob	DATETIME,	-- NULL,
-	gender_code	CHAR(1),	-- NULL,
-	race_code_id	INT,	-- NOT NULL,
-	ethnicity_code_id	INT,	-- NULL,
-	vfc_code_id	INT,	-- NULL,
-	program_close_date	DATETIME,	-- NULL,
-	close_reason_desc	VARCHAR(50),	-- NOT NULL,
-	mother_last_name	VARCHAR(30),	-- NULL,
-	mother_first_name	VARCHAR(25),	-- NULL,
-	birth_state_code_id	INT,	-- NULL,
-	birth_country_code_id	INT,	-- NULL,
-	vaccination_code_id	INT,	-- NOT NULL,
-	vaccination_desc	VARCHAR(100),	-- NOT NULL,
-	vaccination_date	DATETIME,	-- NOT NULL
-	manufacturer_desc	VARCHAR(50),	-- NOT NULL,
-	history_only_flag	CHAR(1),	-- NOT NULL,
-	allergy_risk_desc	VARCHAR(250),	-- NULL,
-	address_line1	VARCHAR(50),	-- NULL,
-	city_code_id	INT,	-- NULL,
-	state_code_id	INT,	-- NULL,
-	zip_code	VARCHAR(10),	-- NULL,
-	county_code_id	INT,	-- NULL,
-	telephone	VARCHAR(30),	-- NULL,
-	email_address	VARCHAR(100),	-- NULL,
-	cell_phone	VARCHAR(30),	-- NULL,
-	audit_update_dttm	DATETIME,	-- NOT NULL,
-	provider_id	INT,	-- identity NOT NULL,
-	provider_desc	VARCHAR(50),	-- NOT NULL,
-	provider_address_line1	VARCHAR(50),	-- NULL,
-	provider_city_code_id	INT,	-- NULL,
-	provider_state_code_id	INT,	-- NULL,
-	provider_zip_code	VARCHAR(10),	-- NULL,
-	provider_telephone	VARCHAR(30),	-- NULL,
-	provider_type_of_practice_code_desc	VARCHAR(35),	-- NOT NULL,
+
+	patient_id	INT, --System Assigned Number used for Primary key. Represents the Patient ID within the system (a.k.a., Web­IZ ID)
+	last_name	VARCHAR(30),	--	Last Name of the Patient
+	first_name	VARCHAR(25),	--	First Name of the patient
+	middle_name	VARCHAR(25),	--	Middle Name of the patient
+	dob	DATETIME,	-- Date of Birth for the patient
+	street_number	VARCHAR(25),	--	The street number portion of the address.
+	street_name	VARCHAR(250),	--	The street name portion of the address.
+	city_desc	VARCHAR(50),	--	Name of the City. Displayed in some dropdowns, reports, etc. within the application.
+	zip_code	VARCHAR(50),	--	The zip code portion of the address
+	county_desc	VARCHAR(50),	--	Name of the County. Displayed in some dropdowns, reports, etc. within the application.
+	race_desc	VARCHAR(50),	--	Full description of the race. Displayed in some dropdowns, reports, etc. within the application.
+	ethnicity_desc	VARCHAR(50),	--	Description of the Ethnicity code. Displayed in some dropdowns, reports, etc. within the application.
+	gender_code	CHAR(1),	--	Identifies the gender of the patient
+	mother_last_name	VARCHAR(30),	--	Last Name of the patient's mother
+	mother_first_name	VARCHAR(25),	--	First name of the patient's mother.
+	mother_maiden_name	VARCHAR(30),	--	Maiden name of the patient's mother.
+	telephone	VARCHAR(30),	--	Home Phone Number for the patient
+	ssn	VARCHAR(11),	--	Social Security Number of the patient
+	local_id	VARCHAR(50),	--	The identifier this clinic has assigned to the patient (MRN)
+	vaccination_desc	VARCHAR(100),	--	Full description of the vaccine
+	vaccination_date	DATETIME,	--	Date the vaccine was administered to the patient
+	patient_vaccination_id	INT,	--	System Assigned Number used for Primary key
+
 
 	source_filename VARCHAR(255),
 
@@ -58,9 +44,11 @@ CREATE TABLE vital.webizs (
 	source_record_number INT,
 
 	imported_at DATETIME
-		CONSTRAINT vital_webizs_imported_at_default DEFAULT CURRENT_TIMESTAMP NOT NULL,
+		CONSTRAINT vital_webizs_imported_at_default
+		DEFAULT CURRENT_TIMESTAMP NOT NULL,
 	imported_to_dw BIT
-		CONSTRAINT vital_webizs_imported_to_dw_default DEFAULT 'FALSE' NOT NULL
+		CONSTRAINT vital_webizs_imported_to_dw_default
+		DEFAULT 'FALSE' NOT NULL
 );
 GO
 
@@ -72,45 +60,29 @@ IF OBJECT_ID('vital.webizs_buffer', 'U') IS NOT NULL
 	DROP TABLE vital.webizs_buffer;
 CREATE TABLE vital.webizs_buffer (
 
-	patient_id	INT,	-- identity NOT NULL,
-	first_name	VARCHAR(25),	-- NOT NULL,
-	middle_name	VARCHAR(25),	-- NULL,
-	last_name	VARCHAR(30),	-- NOT NULL,
-	dob	DATETIME,	-- NULL,
-	gender_code	CHAR(1),	-- NULL,
-	race_code_id	INT,	-- NOT NULL,
-	ethnicity_code_id	INT,	-- NULL,
-	vfc_code_id	INT,	-- NULL,
-	program_close_date	DATETIME,	-- NULL,
-	close_reason_desc	VARCHAR(50),	-- NOT NULL,
-	mother_last_name	VARCHAR(30),	-- NULL,
-	mother_first_name	VARCHAR(25),	-- NULL,
-	birth_state_code_id	INT,	-- NULL,
-	birth_country_code_id	INT,	-- NULL,
-	vaccination_code_id	INT,	-- NOT NULL,
-	vaccination_desc	VARCHAR(100),	-- NOT NULL,
-	vaccination_date	DATETIME,	-- NOT NULL
-	manufacturer_desc	VARCHAR(50),	-- NOT NULL,
-	history_only_flag	CHAR(1),	-- NOT NULL,
-	allergy_risk_desc	VARCHAR(250),	-- NULL,
-	address_line1	VARCHAR(50),	-- NULL,
-	city_code_id	INT,	-- NULL,
-	state_code_id	INT,	-- NULL,
-	zip_code	VARCHAR(10),	-- NULL,
-	county_code_id	INT,	-- NULL,
-	telephone	VARCHAR(30),	-- NULL,
-	email_address	VARCHAR(100),	-- NULL,
-	cell_phone	VARCHAR(30),	-- NULL,
-	audit_update_dttm	DATETIME,	-- NOT NULL,
-	provider_id	INT,	-- identity NOT NULL,
-	provider_desc	VARCHAR(50),	-- NOT NULL,
-	provider_address_line1	VARCHAR(50),	-- NULL,
-	provider_city_code_id	INT,	-- NULL,
-	provider_state_code_id	INT,	-- NULL,
-	provider_zip_code	VARCHAR(10),	-- NULL,
-	provider_telephone	VARCHAR(30),	-- NULL,
-	provider_type_of_practice_code_desc	VARCHAR(35),	-- NOT NULL,
 
+	patient_id	INT, --System Assigned Number used for Primary key. Represents the Patient ID within the system (a.k.a., Web­IZ ID)
+	last_name	VARCHAR(30),	--	Last Name of the Patient
+	first_name	VARCHAR(25),	--	First Name of the patient
+	middle_name	VARCHAR(25),	--	Middle Name of the patient
+	dob	DATETIME,	-- Date of Birth for the patient
+	street_number	VARCHAR(25),	--	The street number portion of the address.
+	street_name	VARCHAR(250),	--	The street name portion of the address.
+	city_desc	VARCHAR(50),	--	Name of the City. Displayed in some dropdowns, reports, etc. within the application.
+	zip_code	VARCHAR(50),	--	The zip code portion of the address
+	county_desc	VARCHAR(50),	--	Name of the County. Displayed in some dropdowns, reports, etc. within the application.
+	race_desc	VARCHAR(50),	--	Full description of the race. Displayed in some dropdowns, reports, etc. within the application.
+	ethnicity_desc	VARCHAR(50),	--	Description of the Ethnicity code. Displayed in some dropdowns, reports, etc. within the application.
+	gender_code	CHAR(1),	--	Identifies the gender of the patient
+	mother_last_name	VARCHAR(30),	--	Last Name of the patient's mother
+	mother_first_name	VARCHAR(25),	--	First name of the patient's mother.
+	mother_maiden_name	VARCHAR(30),	--	Maiden name of the patient's mother.
+	telephone	VARCHAR(30),	--	Home Phone Number for the patient
+	ssn	VARCHAR(11),	--	Social Security Number of the patient
+	local_id	VARCHAR(50),	--	The identifier this clinic has assigned to the patient (MRN)
+	vaccination_desc	VARCHAR(100),	--	Full description of the vaccine
+	vaccination_date	DATETIME,	--	Date the vaccine was administered to the patient
+	patient_vaccination_id	INT,	--	System Assigned Number used for Primary key
 
 
 	source_filename VARCHAR(255),
@@ -121,9 +93,11 @@ CREATE TABLE vital.webizs_buffer (
 	source_record_number INT IDENTITY(1,1),
 
 	imported_at DATETIME
-		CONSTRAINT vital_webizs_buffer_imported_at_default DEFAULT CURRENT_TIMESTAMP NOT NULL,
+		CONSTRAINT vital_webizs_buffer_imported_at_default
+		DEFAULT CURRENT_TIMESTAMP NOT NULL,
 	imported_to_dw BIT
-		CONSTRAINT vital_webizs_buffer_imported_to_dw_default DEFAULT 'FALSE' NOT NULL,
+		CONSTRAINT vital_webizs_buffer_imported_to_dw_default
+		DEFAULT 'FALSE' NOT NULL,
 );
 GO
 
