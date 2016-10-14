8ef96b5d29d4963d252424f84793dc49f4ddaa51
Jake <github@jakewendt.com>
2016-10-14 15:36:10 -0700

    Deving code to check vaccination descriptions.

--- production/webiz_find_undefined_vaccination_desc.sql
+++ production/webiz_find_undefined_vaccination_desc.sql
@@ -12,3 +12,28 @@ LEFT JOIN dbo.dictionary d
 	AND CAST(c.code AS VARCHAR(255)) = vaccination_desc
 WHERE units IS NULL 
 
+
+
+
+
+DELETE FROM dbo.codes WHERE _schema = 'webiz'
+
+DECLARE @bulk_cmd VARCHAR(1000)
+
+ALTER TABLE dbo.codes ADD CONSTRAINT temp_schema_default DEFAULT 'webiz' FOR _schema;
+ALTER TABLE dbo.codes ADD CONSTRAINT temp_table_default DEFAULT 'immunizations' FOR _table;
+ALTER TABLE dbo.codes ADD CONSTRAINT temp_codeset_default DEFAULT 'vaccination_desc' FOR codeset;
+SET @bulk_cmd = 'BULK INSERT dbo.bulk_insert_codes_with_units
+	FROM ''Z:\Renown Project\CHIRP\Personal folders\Jake\chirp\production\content\webiz\immunizations\vaccination_desc.tsv''
+	WITH (
+		ROWTERMINATOR = '''+CHAR(10)+''',
+		FIRSTROW = 2,
+		TABLOCK
+	)';
+	EXEC(@bulk_cmd);
+ALTER TABLE dbo.codes DROP CONSTRAINT temp_schema_default;
+ALTER TABLE dbo.codes DROP CONSTRAINT temp_table_default;
+ALTER TABLE dbo.codes DROP CONSTRAINT temp_codeset_default;
+
+
+
