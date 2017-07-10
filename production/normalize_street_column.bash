c3a21fce7253dc15c0305d6c9007d7651f5912a3
Jake <github@jakewendt.com>
2017-07-10 15:18:43 -0700

    Deving cleaner computed column creation.

--- production/normalize_street_column.bash
+++ production/normalize_street_column.bash
@@ -8,93 +8,164 @@
 table=$1
 field=$2
 
-#	order matters. BOULEVARD contains RD, so before RD extraction"
+#	order matters. BOULEVARD contains RD, so do before RD extraction"
 
-echo
+#	Perhaps I should rewrite this as a loop to make it more clear
+#
+#echo
+#echo "IF COL_LENGTH('${table}','_${field}') IS NOT NULL"
+#echo "	ALTER TABLE ${table} DROP COLUMN _${field};"
+#echo "ALTER TABLE ${table} ADD _${field} AS"
+#echo "	REPLACE( REPLACE( REPLACE( REPLACE( REPLACE( REPLACE("
+#echo "	REPLACE( REPLACE( REPLACE( REPLACE( REPLACE( REPLACE("
+#echo "	REPLACE( REPLACE( REPLACE( REPLACE( REPLACE( REPLACE("
+#echo "	REPLACE( REPLACE( REPLACE( REPLACE( REPLACE( REPLACE("
+#echo "	REPLACE( REPLACE( REPLACE("
+#echo "	REPLACE( REPLACE( REPLACE( REPLACE( REPLACE( ${field}"
+#echo "		,' TRAIL',' '),' TRL',' ')"
+#echo "		,' BOULEVARD',' '),' BLVD',' ')"
+#echo "		,' COURT',' '),' CT',' ')"
+#echo "		,' STREET',' '),' ST',' ')"
+#echo "		,' DRIVE',' '),' DRIV',' '),' DRI',' '),' DR',' ')"
+#echo "		,' ROAD',' '),' RD',' ')"
+#echo "		,' CIRCLE',' '),' CIR',' ')"
+#echo "		,' LANE',' '),' LN',' ')"
+#echo "		,' AVENUE',' '),' AVE',' ')"
+#echo "		,' APT',' '),' UNIT',' ')"
+#echo "		,' MOUNT',' MT'),' PARKWAY',' '),' PKWY',' ')"
+#echo "		,' WAY',' '),' PL',' ')"
+#echo "		,'SOUTH','S'),'NORTH','N'),'EAST','E'),'WEST','W'),' ','')"
+#echo "	PERSISTED;"
+#echo
+#
+#echo
+#echo "IF COL_LENGTH('${table}','_${field}_prehash') IS NOT NULL"
+#echo "	ALTER TABLE ${table} DROP COLUMN _${field}_prehash;"
+#echo "ALTER TABLE ${table} ADD _${field}_prehash AS"
+#echo "	REPLACE( REPLACE( REPLACE( REPLACE( REPLACE( REPLACE("
+#echo "	REPLACE( REPLACE( REPLACE( REPLACE( REPLACE( REPLACE("
+#echo "	REPLACE( REPLACE( REPLACE( REPLACE( REPLACE( REPLACE("
+#echo "	REPLACE( REPLACE( REPLACE( REPLACE( REPLACE( REPLACE("
+#echo "	REPLACE( REPLACE( REPLACE("
+#echo "	REPLACE( REPLACE( REPLACE( REPLACE( REPLACE("
+#echo "		SUBSTRING( ${field}, 1,"
+#echo "			ISNULL(NULLIF(CHARINDEX('#',${field})-1,-1),LEN(${field})))"
+#echo "		,' TRAIL',' '),' TRL',' ')"
+#echo "		,' BOULEVARD',' '),' BLVD',' ')"
+#echo "		,' COURT',' '),' CT',' ')"
+#echo "		,' STREET',' '),' ST',' ')"
+#echo "		,' DRIVE',' '),' DRIV',' '),' DRI',' '),' DR',' ')"
+#echo "		,' ROAD',' '),' RD',' ')"
+#echo "		,' CIRCLE',' '),' CIR',' ')"
+#echo "		,' LANE',' '),' LN',' ')"
+#echo "		,' AVENUE',' '),' AVE',' ')"
+#echo "		,' APT',' '),' UNIT',' ')"
+#echo "		,' MOUNT',' MT'),' PARKWAY',' '),' PKWY',' ')"
+#echo "		,' WAY',' '),' PL',' ')"
+#echo "		,'SOUTH','S'),'NORTH','N'),'EAST','E'),'WEST','W'),' ','')"
+#echo "	PERSISTED;"
+#echo
+#
+#
+#
+#
+#echo
+#echo "IF COL_LENGTH('${table}','_${field}_precomma') IS NOT NULL"
+#echo "	ALTER TABLE ${table} DROP COLUMN _${field}_precomma;"
+#echo "ALTER TABLE ${table} ADD _${field}_precomma AS"
+#echo "	REPLACE( REPLACE( REPLACE( REPLACE( REPLACE( REPLACE("
+#echo "	REPLACE( REPLACE( REPLACE( REPLACE( REPLACE( REPLACE("
+#echo "	REPLACE( REPLACE( REPLACE( REPLACE( REPLACE( REPLACE("
+#echo "	REPLACE( REPLACE( REPLACE( REPLACE( REPLACE( REPLACE("
+#echo "	REPLACE( REPLACE( REPLACE("
+#echo "	REPLACE( REPLACE( REPLACE( REPLACE( REPLACE("
+#echo "		SUBSTRING( ${field}, 1,"
+#echo "			ISNULL(NULLIF(CHARINDEX(',',${field})-1,-1),LEN(${field})))"
+#echo "		,' TRAIL',' '),' TRL',' ')"
+#echo "		,' BOULEVARD',' '),' BLVD',' ')"
+#echo "		,' COURT',' '),' CT',' ')"
+#echo "		,' STREET',' '),' ST',' ')"
+#echo "		,' DRIVE',' '),' DRIV',' '),' DRI',' '),' DR',' ')"
+#echo "		,' ROAD',' '),' RD',' ')"
+#echo "		,' CIRCLE',' '),' CIR',' ')"
+#echo "		,' LANE',' '),' LN',' ')"
+#echo "		,' AVENUE',' '),' AVE',' ')"
+#echo "		,' APT',' '),' UNIT',' ')"
+#echo "		,' MOUNT',' MT'),' PARKWAY',' '),' PKWY',' ')"
+#echo "		,' WAY',' '),' PL',' ')"
+#echo "		,'SOUTH','S'),'NORTH','N'),'EAST','E'),'WEST','W'),' ','')"
+#echo "	PERSISTED;"
+#echo
+#
+
+add_nest(){
+	nest="REPLACE(${*},' TRAIL',' ')"
+	nest="REPLACE(${nest},' TRL',' ')"
+#	order matters. BOULEVARD contains RD, so do before RD extraction"
+	nest="REPLACE(${nest},' BOULEVARD',' ')"
+	nest="REPLACE(${nest},' BLVD',' ')"
+	nest="REPLACE(${nest},' COURT',' ')"
+	nest="REPLACE(${nest},' CT',' ')"
+	nest="REPLACE(${nest},' DRIVE',' ')"
+	nest="REPLACE(${nest},' DRIV',' ')"
+	nest="REPLACE(${nest},' DR',' ')"
+	nest="REPLACE(${nest},' ROAD',' ')"
+	nest="REPLACE(${nest},' RD',' ')"
+	nest="REPLACE(${nest},' CIRCLE',' ')"
+	nest="REPLACE(${nest},' LANE',' ')"
+	nest="REPLACE(${nest},' LN',' ')"
+	nest="REPLACE(${nest},' AVENUE',' ')"
+	nest="REPLACE(${nest},' AVE',' ')"
+	nest="REPLACE(${nest},' APT',' ')"
+	nest="REPLACE(${nest},' UNIT',' ')"
+	nest="REPLACE(${nest},' MOUNT',' MT')"		#	why keep?
+	nest="REPLACE(${nest},' PARKWAY',' ')"
+	nest="REPLACE(${nest},' PKWY',' ')"
+	nest="REPLACE(${nest},' WAY',' ')"
+	nest="REPLACE(${nest},' PL',' ')"
+	nest="REPLACE(${nest},'NORTH','N')"
+	nest="REPLACE(${nest},'SOUTH','S')"
+	nest="REPLACE(${nest},'EAST','E')"
+	nest="REPLACE(${nest},'WEST','W')"
+	nest="REPLACE(${nest},'SS','S')"
+	nest="REPLACE(${nest},'OO','O')"
+	nest="REPLACE(${nest},' ','')"
+	echo $nest
+}
+
+
+s=$(add_nest "${field}")
+echo 
 echo "IF COL_LENGTH('${table}','_${field}') IS NOT NULL"
 echo "	ALTER TABLE ${table} DROP COLUMN _${field};"
 echo "ALTER TABLE ${table} ADD _${field} AS"
-echo "	REPLACE( REPLACE( REPLACE( REPLACE( REPLACE( REPLACE("
-echo "	REPLACE( REPLACE( REPLACE( REPLACE( REPLACE( REPLACE("
-echo "	REPLACE( REPLACE( REPLACE( REPLACE( REPLACE( REPLACE("
-echo "	REPLACE( REPLACE( REPLACE( REPLACE( REPLACE( REPLACE("
-echo "	REPLACE( REPLACE( REPLACE("
-echo "	REPLACE( REPLACE( REPLACE( REPLACE( REPLACE( ${field}"
-echo "		,' TRAIL',' '),' TRL',' ')"
-echo "		,' BOULEVARD',' '),' BLVD',' ')"
-echo "		,' COURT',' '),' CT',' ')"
-echo "		,' STREET',' '),' ST',' ')"
-echo "		,' DRIVE',' '),' DRIV',' '),' DRI',' '),' DR',' ')"
-echo "		,' ROAD',' '),' RD',' ')"
-echo "		,' CIRCLE',' '),' CIR',' ')"
-echo "		,' LANE',' '),' LN',' ')"
-echo "		,' AVENUE',' '),' AVE',' ')"
-echo "		,' APT',' '),' UNIT',' ')"
-echo "		,' MOUNT',' MT'),' PARKWAY',' '),' PKWY',' ')"
-echo "		,' WAY',' '),' PL',' ')"
-echo "		,'SOUTH','S'),'NORTH','N'),'EAST','E'),'WEST','W'),' ','')"
+echo $s
 echo "	PERSISTED;"
 echo
 
-echo
+#	Take everything up to first , (comma)
+
+#	Take everything up to first # (hash)
+s="SUBSTRING( ${field}, 1, ISNULL(NULLIF(CHARINDEX('#',${field})-1,-1),LEN(${field})))"
+s=$(add_nest "$s")
+echo 
 echo "IF COL_LENGTH('${table}','_${field}_prehash') IS NOT NULL"
 echo "	ALTER TABLE ${table} DROP COLUMN _${field}_prehash;"
 echo "ALTER TABLE ${table} ADD _${field}_prehash AS"
-echo "	REPLACE( REPLACE( REPLACE( REPLACE( REPLACE( REPLACE("
-echo "	REPLACE( REPLACE( REPLACE( REPLACE( REPLACE( REPLACE("
-echo "	REPLACE( REPLACE( REPLACE( REPLACE( REPLACE( REPLACE("
-echo "	REPLACE( REPLACE( REPLACE( REPLACE( REPLACE( REPLACE("
-echo "	REPLACE( REPLACE( REPLACE("
-echo "	REPLACE( REPLACE( REPLACE( REPLACE( REPLACE("
-echo "		SUBSTRING( ${field}, 1,"
-echo "			ISNULL(NULLIF(CHARINDEX('#',${field})-1,-1),LEN(${field})))"
-echo "		,' TRAIL',' '),' TRL',' ')"
-echo "		,' BOULEVARD',' '),' BLVD',' ')"
-echo "		,' COURT',' '),' CT',' ')"
-echo "		,' STREET',' '),' ST',' ')"
-echo "		,' DRIVE',' '),' DRIV',' '),' DRI',' '),' DR',' ')"
-echo "		,' ROAD',' '),' RD',' ')"
-echo "		,' CIRCLE',' '),' CIR',' ')"
-echo "		,' LANE',' '),' LN',' ')"
-echo "		,' AVENUE',' '),' AVE',' ')"
-echo "		,' APT',' '),' UNIT',' ')"
-echo "		,' MOUNT',' MT'),' PARKWAY',' '),' PKWY',' ')"
-echo "		,' WAY',' '),' PL',' ')"
-echo "		,'SOUTH','S'),'NORTH','N'),'EAST','E'),'WEST','W'),' ','')"
+echo $s
 echo "	PERSISTED;"
 echo
 
-
-
-
-echo
+#	Take everything up to first , (comma)
+s="SUBSTRING( ${field}, 1, ISNULL(NULLIF(CHARINDEX(',',${field})-1,-1),LEN(${field})))"
+s=$(add_nest "$s")
+echo 
 echo "IF COL_LENGTH('${table}','_${field}_precomma') IS NOT NULL"
 echo "	ALTER TABLE ${table} DROP COLUMN _${field}_precomma;"
 echo "ALTER TABLE ${table} ADD _${field}_precomma AS"
-echo "	REPLACE( REPLACE( REPLACE( REPLACE( REPLACE( REPLACE("
-echo "	REPLACE( REPLACE( REPLACE( REPLACE( REPLACE( REPLACE("
-echo "	REPLACE( REPLACE( REPLACE( REPLACE( REPLACE( REPLACE("
-echo "	REPLACE( REPLACE( REPLACE( REPLACE( REPLACE( REPLACE("
-echo "	REPLACE( REPLACE( REPLACE("
-echo "	REPLACE( REPLACE( REPLACE( REPLACE( REPLACE("
-echo "		SUBSTRING( ${field}, 1,"
-echo "			ISNULL(NULLIF(CHARINDEX(',',${field})-1,-1),LEN(${field})))"
-echo "		,' TRAIL',' '),' TRL',' ')"
-echo "		,' BOULEVARD',' '),' BLVD',' ')"
-echo "		,' COURT',' '),' CT',' ')"
-echo "		,' STREET',' '),' ST',' ')"
-echo "		,' DRIVE',' '),' DRIV',' '),' DRI',' '),' DR',' ')"
-echo "		,' ROAD',' '),' RD',' ')"
-echo "		,' CIRCLE',' '),' CIR',' ')"
-echo "		,' LANE',' '),' LN',' ')"
-echo "		,' AVENUE',' '),' AVE',' ')"
-echo "		,' APT',' '),' UNIT',' ')"
-echo "		,' MOUNT',' MT'),' PARKWAY',' '),' PKWY',' ')"
-echo "		,' WAY',' '),' PL',' ')"
-echo "		,'SOUTH','S'),'NORTH','N'),'EAST','E'),'WEST','W'),' ','')"
+echo $s
 echo "	PERSISTED;"
 echo
-
 
 
 
