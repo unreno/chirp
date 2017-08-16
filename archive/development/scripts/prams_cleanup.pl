27bbe43b2bd460a947c16858b2795fa664d09e8e
Jake <github@jakewendt.com>
2017-08-16 14:52:46 -0700

    Merged 2 calls.

--- archive/development/scripts/prams_cleanup.pl
+++ archive/development/scripts/prams_cleanup.pl
@@ -12,6 +12,8 @@ while (<>) {
 	#	merge if field starts with a space ,something, else, => ,"something, else",
 	#s/,([^,]+), ([^,]+),/,"$1, $2",/g;	#	nope, too many false joins
 
+	#	The /x modifier permits multiline regex and inline comments.
+
 	s/,(False|True),
 		([^,]*),(False|True),
 		([^,]*),([^,]*),(False|True),
@@ -24,38 +26,54 @@ while (<>) {
 	/,$1,$2,$3,$4,$5,$6,$7,$8,$9,$10,"$11",$12,$13,$14,$15,$16,$17,$18,$19,/x;
 
 	s/,(Yes|No|Unknown),(Yes|No|Unknown),(Yes|No|Unknown),(Yes|No|Unknown),([^,]*),
-		(.*?),												# 50 AX BirthHospitalFacility VARCHAR(50) can contain commas
+		(.*?),												#	50 AX BirthHospitalFacility VARCHAR(50) can contain commas
 		([^,]*),([^,]*),([^,]*),(False|True),
-		(.*?),												# 55 BC CollectionFacilityFacility can contain commas
+		(.*?),												#	55 BC CollectionFacilityFacility can contain commas
 		([^,]*),([^,]*),([^,]*),(False|True),
-		(.*?),												# 60 BH ReportToFacility can contain commas
+		(.*?),												#	60 BH ReportToFacility can contain commas
 		([^,]*),([^,]*),([^,]*),(False|True),([^,]*),([^,]*),
 		(NV|NEVADA|AZ|CA|UT),
 	/,$1,$2,$3,$4,$5,"$6",$7,$8,$9,$10,"$11",$12,$13,$14,$15,"$16",$17,$18,$19,$20,$21,$22,$23,/x;
 
-	#	Do this before the next one as the next one adds double quotes
-	s/,(NV|NEVADA|AZ|CA|UT),						# 67 BO ReportToState VARCHAR(10)
-		([^,]*),([^,]*),([^,]*),([^,]*),([^,]*),([^,]*),([^,]*),
-		(NV|NEVADA|CA|NY|AZ|UT),					# 75 BW MotherState VARCHAR(5)
-		([0-9\\]*),													#	76 MotherZip VARCHAR(10)
+#	#	Do this before the next one as the next one adds double quotes
+#	s/,(NV|NEVADA|AZ|CA|UT),						#	67 BO ReportToState VARCHAR(10)
+#		([^,]*),([^,]*),([^,]*),([^,]*),([^,]*),([^,]*),([^,]*),
+#		(NV|NEVADA|CA|NY|AZ|UT),					#	75 BW MotherState VARCHAR(5)
+#		([0-9\\]*),													#	76 MotherZip VARCHAR(10)
+#		([0-9]*),													#	77 Phone VARCHAR(10)
+#		([0-9]*),													#	78 BZ EmergencyPhone VARCHAR(10)
+#		([A-z \*\#\/\.\-\(\)\']+),([A-z \*\#\/\.\-\(\)\']+),
+#																			#	79 FatherName VARCHAR(30) can contain commas
+#	/,$1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,"$13,$14",/x;
+#
+#	s/,(NV|NEVADA|AZ|CA|UT),						#	67 BO ReportToState VARCHAR(10)
+#		([0-9\-\\]+),([^,]*),([^,]*),([^,]*),([^,]*),
+#																			#	68 ReportToZip VARCHAR(10)
+#																			#	69 MotherLastName VARCHAR(20)
+#																			#	70 MotherFirstName VARCHAR(20)
+#																			#	71 BS MotherDOB DATE
+#																			#	72 MotherMaidenName VARCHAR(10)
+##		(.*?),														#	73 BU MotherAddress VARCHAR(30)	can contain commas
+#		([^,]*),													#	74 MotherCity VARCHAR(30)
+#		(NV|NEVADA|CA|NY|AZ|UT),					#	75 BW MotherState VARCHAR(5) 5? likely never actually NEVADA
+#		([0-9\-\\]+),
+#	/,$1,$2,$3,$4,$5,$6,"$7",$8,$9,$10,/x;
+
+	s/,(HI|NV|NEVADA|AZ|CA|UT),					#	67 BO ReportToState VARCHAR(10)
+		([0-9\-\\]+),											#	68 ReportToZip VARCHAR(10)
+		([^,]*),													#	69 MotherLastName VARCHAR(20)
+		([^,]*),													#	70 MotherFirstName VARCHAR(20)
+		([^,]*),													#	71 BS MotherDOB DATE
+		([^,]*),													#	72 MotherMaidenName VARCHAR(10)
+		(.*?),														#	73 BU MotherAddress VARCHAR(30)	can contain commas
+		([^,]*),													#	74 MotherCity VARCHAR(30)
+		(HI|NV|NEVADA|CA|NY|AZ|UT),				#	75 BW MotherState VARCHAR(5) 5? likely never actually NEVADA
+		([0-9\-\\]*),											#	76 MotherZip VARCHAR(10)
 		([0-9]*),													#	77 Phone VARCHAR(10)
 		([0-9]*),													#	78 BZ EmergencyPhone VARCHAR(10)
 		([A-z \*\#\/\.\-\(\)\']+),([A-z \*\#\/\.\-\(\)\']+),
 																			#	79 FatherName VARCHAR(30) can contain commas
-	/,$1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,"$13,$14",/x;
-
-	s/,(NV|NEVADA|AZ|CA|UT),						#	67 BO ReportToState VARCHAR(10)
-		([0-9\-\\]+),([^,]*),([^,]*),([^,]*),([^,]*),
-																			#	68 ReportToZip VARCHAR(10)
-																			#	69 MotherLastName VARCHAR(20)
-																			#	70 MotherFirstName VARCHAR(20)
-																			#	71 BS MotherDOB DATE
-																			#	72 MotherMaidenName VARCHAR(10)
-		(.*?),														#	73 BU MotherAddress VARCHAR(30)	can contain commas
-		([^,]*),													#	74 MotherCity VARCHAR(30)
-		(NV|NEVADA|CA|NY|AZ|UT),					#	75 BW MotherState VARCHAR(5) 5? likely never actually NEVADA
-		([0-9\-\\]+),
-	/,$1,$2,$3,$4,$5,$6,"$7",$8,$9,$10,/x;
+	/,$1,$2,$3,$4,$5,$6,"$7",$8,$9,$10,$11,$12,"$13,$14",/x;
 
 } continue {
 	print;
